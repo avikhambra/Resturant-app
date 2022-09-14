b626838c06a03146efb8512b79dfcd90b83219e2
Abhisekh khambra <kumaravi422@gmail.com>
2022-09-14 18:55:13 +0530

    Resturant app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// React arrow function componet export (rafce)
+
+import React from 'react'
+import Resturant from './Component/Basic/Resturant'
+
+const App = () => {
+  return (
+    <div>
+       <Resturant/>
+    </div>
+  )
+}
+
+
+export default App
---
+++ src/Component/Basic/menuApi.js
@@ -0,0 +1,130 @@
+const Menu = [
+    {
+      id: 1,
+      image: "https://im.rediff.com/getahead/2020/sep/29burnt-garlic-chilli-maggi.jpg",
+      name: "Maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "https://ikneadtoeat.com/wp-content/uploads/2022/01/aloo-pakora-recipe-new-2.jpg",
+      name: "Allu pakoida",
+      category: "breakfast",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "https://www.thefieryvegetarian.com/wp-content/uploads/2022/04/masala-corn.jpg",
+      name: "Corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "https://cdn.tasteatlas.com/Images/Dishes/43f61ad02971442ba119a646818e72cc.jpg",
+      name: "Daal Chaval",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "https://img.onmanorama.com/content/dam/mm/en/food/features/images/2021/10/17/pizza.jpg",
+      name: "Pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "https://previews.123rf.com/images/witty234/witty2340912/witty234091200181/6404895-non-vegetarian-thali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "https://1.bp.blogspot.com/-Yn52GtCFZas/X6ft5y-LO3I/AAAAAAAAStI/sq87NFdMRyYH_g_8Vrg2_LZkMa-HPtHTACLcBGAsYHQ/w1200-h630-p-k-no-nu/Diwali%2BSweets.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "https://www.indianfoodforever.com/iffwd/wp-content/uploads/rajma-chawal.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "https://static.toiimg.com/thumb/61050397.cms?width=1200&height=900",
+      name: "Samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+
+    {
+        id: 10,
+        image: "https://cdn1.foodviva.com/static-content/food-images/snacks-recipes/pani-puri/pani-puri.jpg",
+        name: "Pani Puri",
+        category: "evening",
+        price: "10₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+
+      {
+        id: 11,
+        image: "https://static.toiimg.com/photo/84667091.cms",
+        name: "Mutton",
+        category: "dinner",
+        price: "10₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+
+      {
+        id: 11,
+        image: "https://images.indulgexpress.com/uploads/user/imagelibrary/2022/6/16/original/CholebhatureisanintrinsicpartofDelhisstreetfood.jpg",
+        name: "Chole bhatura",
+        category: "lunch",
+        price: "10₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+
+      
+      {
+        id: 12,
+        image: "https://ychef.files.bbci.co.uk/976x549/p04tx3m6.jpg",
+        name: "Light Food",
+        category: "breakfast",
+        price: "10₹",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      },
+
+
+
+  ];
+  
+  export default Menu;
